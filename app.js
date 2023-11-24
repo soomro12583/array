4210287ced324ee6cf491cade22e9e50412aacaa
soomro12583 <nadeemkhanasf909@mail.com>
2023-11-24 12:45:39 +0500

    practice

---
+++ app.js
@@ -0,0 +1,7 @@
+/*let subject1 = "Math";
+let subject2 = "English";
+let subject3 = "Physics";
+let subject4 = "Chemistry";*/
+
+let subject = ["Math"," English", " Physics", " Chemistry"];
+document.write(subject[3]);
