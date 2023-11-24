32d26fa063ae5f0b05ec12d4487ade4922dee756
soomro12583 <nadeemkhanasf909@mail.com>
2023-11-24 15:23:57 +0500

    practice

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ let subject2 = "English";
 let subject3 = "Physics";
 let subject4 = "Chemistry";*/
 
-let subject = ["Math"," English", " Physics", " Chemistry"];
-document.write(subject[3]);
+/*let subject = ["Math"," English", " Physics", " Chemistry"];
+document.write(subject[3]);*/
+
+let devices = [];
+
+devices.push("keyboard");
+devices.push("Mouse");
+devices.push("Printer");
+devices.push("Monitor");
+
+document.write( "<b> Devices </b> <br> <br>" + devices);
+document.write("<br> out:" + devices.shift());
+document.write("<br> out:" + devices.shift());
+document.write("<br> out:" + devices.shift());
+document.write("<br> out:" + devices.shift());
