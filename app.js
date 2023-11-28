62094a4638179b0333f707f7e0110bb5e1d826c4
soomro12583 <nadeemkhanasf909@mail.com>
2023-11-28 08:12:23 +0500

    Prectice

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let subject4 = "Chemistry";*/
 /*let subject = ["Math"," English", " Physics", " Chemistry"];
 document.write(subject[3]);*/
 
-let devices = [];
+/*let devices = [];
 
 devices.push("keyboard");
 devices.push("Mouse");
@@ -17,4 +17,20 @@ document.write( "<b> Devices </b> <br> <br>" + devices);
 document.write("<br> out:" + devices.shift());
 document.write("<br> out:" + devices.shift());
 document.write("<br> out:" + devices.shift());
-document.write("<br> out:" + devices.shift());
+document.write("<br> out:" + devices.shift());*/
+
+
+/*var name = prompt();
+alert(name);*/
+
+/*var enterName = prompt("Plase Enter your Name");
+var cast = prompt("Please enter your Cast");*/
+const  name = "Nadeem";
+//alert(name);
+//console.log (name);
+//document.write (name);
+//var wieght = 150;
+//var num = 7;
+var originalNum = 23;
+var newNum = originalNum+7
+document.write(newNum+originalNum);
