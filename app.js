c02fc29f3c58f4e6d8f02dcee67f389c58ff24a8
soomro12583 <nadeemkhanasf909@mail.com>
2023-12-19 08:47:53 +0500

    prectice

--- app.js
+++ app.js
@@ -25,12 +25,21 @@ alert(name);*/
 
 /*var enterName = prompt("Plase Enter your Name");
 var cast = prompt("Please enter your Cast");*/
-const  name = "Nadeem";
+//const  name = "Nadeem";
 //alert(name);
 //console.log (name);
 //document.write (name);
 //var wieght = 150;
 //var num = 7;
-var originalNum = 23;
+/*var originalNum = 23;
 var newNum = originalNum+7
-document.write(newNum+originalNum);
+document.write(newNum+originalNum);*/
+var enterName = prompt("Please Enter Your Name");
+var phone = prompt("Please Enter Your Phone Number");
+/*var email = prompt("Please Enter Your email");
+var pass = prompt("Please Enter Your Password");
+var conform = prompt("Please Enter Your Conform Password");*/
+document.write(enterName);
+document.write(phone);
+var name = prompt("what is your name?");
+document.write(name);
